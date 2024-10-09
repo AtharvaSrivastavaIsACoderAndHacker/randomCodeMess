8e7717417196d96c125b59a591bb4cdba6a546db
Atharva Srivastava <atharva201004@gmail.com>
2024-10-09 23:31:25 +0530

    9/10/24 23:31 ++1CFile(cIntMacrosMess.c)

---
+++ cIntMacrosMess.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdint.h>
+
+int main(int argc, char const *argv[]){
+    
+    int8_t x = 33333; // limit exceeded
+    (void)printf("%d\n",INT16_MAX);    
+    
+    return 0;
+}
